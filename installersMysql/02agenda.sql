baf3a59abcf7ac17b86371383a91b03dea6405ae
wildmice <47729395+wildmice@users.noreply.github.com>
2025-05-22 09:56:44 -0600

    DB Creacion de Schema y Tabla

---
+++ installersMysql/02agenda.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `cafira`.`agenda` (
+  `idagenda` INT NOT NULL,
+  `Nombre` VARCHAR(70) NOT NULL,
+  `Telefono` VARCHAR(45) NOT NULL,
+  `Fecha de Nacimiento` DATE NOT NULL,
+  PRIMARY KEY (`idagenda`));
