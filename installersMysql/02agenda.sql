80aeb7f5748e30c0f94b9a2a19060806c04720fe
Gerardo Alcalà Dàvalos <wildmice@hotmail.com>
2025-05-22 19:55:55 -0600

    ajuste en query 02agenda.sql

--- installersMysql/02agenda.sql
+++ installersMysql/02agenda.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `cafira`.`agenda` (
-  `idagenda` INT NOT NULL,
+  `idagenda` INT NOT NULL AUTO_INCREMENT,
   `Nombre` VARCHAR(70) NOT NULL,
   `Telefono` VARCHAR(45) NOT NULL,
-  `Fecha de Nacimiento` DATE NOT NULL,
+  `Fecha Nac` DATE NOT NULL,
   PRIMARY KEY (`idagenda`));
